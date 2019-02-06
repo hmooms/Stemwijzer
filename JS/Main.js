ffebb89122420ad4d15724b95b3003eb87b7d4c4
Hugo PC <99046763@mydavinci.nl>
2019-02-06 17:40:35 +0100

    no message

--- JS/Main.js
+++ JS/Main.js
@@ -5,10 +5,13 @@ var startPage = document.getElementById('intro');
 var questionPage = document.getElementById('questions');
 var selectImportant = document.getElementById('important-statements');
 var selectPartiesPage = document.getElementById('select-parties');
-
 var agree = document.getElementById('pro');
 var ambivalent = document.getElementById('ambivalent');
 var disagree = document.getElementById('contra');
+var title = document.getElementById('title');
+var statement = document.getElementById('statement');
+
+// add html
 
 // event listeners
 document.getElementById('start').addEventListener('click', start);
@@ -19,9 +22,11 @@ ambivalent.addEventListener('click', function(){nextPage('ambivialent')});
 disagree.addEventListener('click', function(){nextPage('contra')});
 
 function start(){
+
     startPage.style.display = "none";
     questionPage.style.display = "block";
     question = 0;
+    loadStatement();
 }
 
 function intro(){
@@ -33,20 +38,28 @@ function selectParties(){
     questionPage.style.display = "block";
 }
 
+function loadStatement(){
+    console.log(subjects.length);
+    title.innerHTML = subjects[question].title;
+    statement.innerHTML = subjects[question].statement;
+}
+
 function nextPage(answer){
     choices[question] = answer;
-    if(question = subjects.length){
+    if(question == subjects.length){
         selectParties();
     } else{
         question++;
+        loadStatement();
     }
 }
 
 function previousPage(){
-    if(question = 0){
+    if(question == 0){
         intro();
     } else{
         question--;
+        loadStatement();
     }
 }
 
