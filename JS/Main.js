50eb4a55dcfddc32673311657ccb67a9d390be07
99046763 <99046763@mydavinci.nl>
2019-02-09 09:50:14 +0100

    no message

--- JS/Main.js
+++ JS/Main.js
@@ -1,66 +1,80 @@
 // variables
 var choices = [];
-var question;
+var page;
 var startPage = document.getElementById('intro');
 var questionPage = document.getElementById('questions');
 var selectImportant = document.getElementById('important-statements');
-var selectPartiesPage = document.getElementById('select-parties');
+var selectPartiesPage = document.getElementById('parties');
 var agree = document.getElementById('pro');
 var ambivalent = document.getElementById('ambivalent');
 var disagree = document.getElementById('contra');
 var title = document.getElementById('title');
 var statement = document.getElementById('statement');
-
-// add html
+var backbtn = document.getElementById('back');
 
 // event listeners
 document.getElementById('start').addEventListener('click', start);
-document.getElementById('back').addEventListener('click', previousPage);
-document.getElementById('skip').addEventListener('click', function(){nextPage(0)});
-agree.addEventListener('click', function(){nextPage('pro')});
-ambivalent.addEventListener('click', function(){nextPage('ambivialent')});
-disagree.addEventListener('click', function(){nextPage('contra')});
+document.getElementById('skip').addEventListener('click', function(){goToNextPage(0)});
+backbtn.addEventListener('click', goToPreviousPage);
+agree.addEventListener('click', function(){goToNextPage('pro')});
+ambivalent.addEventListener('click', function(){goToNextPage('ambivialent')});
+disagree.addEventListener('click', function(){goToNextPage('contra')});
 
 function start(){
-
     startPage.style.display = "none";
-    questionPage.style.display = "block";
-    question = 0;
+    backbtn.style.display = "block";
+    page = 0;
     loadStatement();
 }
 
-function intro(){
+function loadIntro(){
     startPage.style.display = "block";
     questionPage.style.display = "none";
+    backbtn.style.display = "none";
 }
 
-function selectParties(){
-    questionPage.style.display = "block";
+function loadSelectParties(){
+    questionPage.style.display = "none";
+    selectPartiesPage.style.display = "block";
+    loadParties();
 }
 
 function loadStatement(){
+    console.log(page);
+    questionPage.style.display = "block";
+    title.innerHTML = subjects[page].title;
+    statement.innerHTML = subjects[page].statement;
+}
+
+function loadParties(){
+    parties.forEach(party => {
+        document.getElementById('test').innerHTML += party.name;       
+    });
+}
+
+function goToNextPage(answer){
+    choices[page] = answer;
+    page++
     console.log(subjects.length);
-    title.innerHTML = subjects[question].title;
-    statement.innerHTML = subjects[question].statement;
-}
-
-function nextPage(answer){
-    choices[question] = answer;
-    if(question == subjects.length){
-        selectParties();
+    if(page == subjects.length){
+        loadSelectParties();
     } else{
-        question++;
         loadStatement();
     }
 }
 
-function previousPage(){
-    if(question == 0){
-        intro();
+function goToPreviousPage(){
+    page--;
+    if(page == -1){
+        loadIntro();
+    } 
+    // else if(importantquestions == "block") {
+    //    loadStatement()
+    // } 
+    else if(selectPartiesPage.style.display == "block"){
+        selectPartiesPage.style.display = "none";
+        loadStatement();
     } else{
-        question--;
         loadStatement();
     }
 }
-
-
