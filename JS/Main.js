78ec5593a459de350a1d188f628ff1cd49e8d0d2
99046763 <99046763@mydavinci.nl>
2019-02-12 14:31:10 +0100

    grote stappen gemaakt vandaag :)

--- JS/Main.js
+++ JS/Main.js
@@ -1,25 +1,36 @@
-// variables
+// #region variables / arrays
 var choices = [];
+var selectedStatements = [];
 var selectedParties = [];
 var page;
+var title = document.getElementById('title');
+var statement = document.getElementById('statement');
+// #endregion
+
+// #region page variables
 var startPage = document.getElementById('intro');
 var questionPage = document.getElementById('questions');
-var selectImportant = document.getElementById('important-statements');
+var selectImportantPage = document.getElementById('important-statements');
 var selectPartiesPage = document.getElementById('parties');
+var resultsPage = document.getElementById('results');
+// #endregion
+
+// #region btn variables
 var agree = document.getElementById('pro');
 var ambivalent = document.getElementById('ambivalent');
 var disagree = document.getElementById('contra');
-var title = document.getElementById('title');
-var statement = document.getElementById('statement');
 var backbtn = document.getElementById('back');
+// #endregion
 
-// event listeners
+// #region event listeners
 document.getElementById('start').addEventListener('click', start);
 document.getElementById('skip').addEventListener('click', function(){goToNextPage(0)});
+document.getElementById('go-to-parties').addEventListener('click', function(){submitToArray('statements')});
 backbtn.addEventListener('click', goToPreviousPage);
 agree.addEventListener('click', function(){goToNextPage('pro')});
-ambivalent.addEventListener('click', function(){goToNextPage('ambivialent')});
+ambivalent.addEventListener('click', function(){goToNextPage('ambivalent')});
 disagree.addEventListener('click', function(){goToNextPage('contra')});
+// #endregion
 
 function start(){
     startPage.style.display = "none";
@@ -34,33 +45,73 @@ function loadIntro(){
     backbtn.style.display = "none";
 }
 
-function loadSelectPartiesPage(){
-    questionPage.style.display = "none";
-    selectPartiesPage.style.display = "block";
-    loadParties();
-}
-
 function loadStatement(){
     questionPage.style.display = "block";
     title.innerHTML = subjects[page].title;
     statement.innerHTML = subjects[page].statement;
+    loadButtonColor();
 }
 
-function loadParties(){
-    document.getElementById('test').innerHTML = "";
-    parties.forEach(party => {
-        document.getElementById('test').innerHTML += '<p class="w3-third"><input type="checkbox" value="' + party.name + '" class="choose-party w3-check"><label> ' + party.name + '</label></p>';       
+function loadSelectStatementsPage(){
+    questionPage.style.display = "none";
+    selectImportantPage.style.display = "block"
+    loadStatements();
+}
+
+function loadStatements() {
+    document.getElementById('statements').innerHTML = "";
+    subjects.forEach(subject => {
+        document.getElementById('statements').innerHTML += '<p class="w3-third"><input type="checkbox" value="' + subject.title + '" class="w3-check"><label> ' + subject.title + '</label></p>';
     });
 }
 
-function loadResultsPage(){
-
+function loadSelectPartiesPage(){
+    selectImportantPage.style.display = "none";
+    selectPartiesPage.style.display = "block";
+    loadParties();
 }
 
+function loadResultsPage(){
+    selectPartiesPage.style.display = "none";
+    resultPage.style.display = "block";
+}
+
+function loadButtonColor() {
+    ambivalent.classList.replace('w3-green', 'w3-grey');
+    agree.classList.replace('w3-green', 'w3-grey');
+    disagree.classList.replace('w3-green', 'w3-grey');
+    if (choices[page] == "pro" || "ambivalent" || "contra") {
+        document.getElementById(choices[page]).classList.add("w3-green");
+        document.getElementById(choices[page]).classList.remove('w3-grey');
+    }
+}
+
+function loadParties(){
+    document.getElementById('select-parties').innerHTML = "";
+    parties.forEach(party => {
+        document.getElementById('select-parties').innerHTML += '<p class="w3-third"><input type="checkbox" value="' + party.name + '" class="w3-check"><label> ' + party.name + ' (' + party.size + ')' + '</label></p>';       
+    });
+}
+
+function submitToArray(array){
+    var inputs = document.getElementsByClassName('w3-check');
+    for(i = 0; i < inputs.length; i++){
+        if(inputs[i].checked){
+            ((array == "statements")? selectedStatements : selectedParties).push(inputs[i].value);
+        }
+    }
+    goToNextPage();
+}
+ 
 function goToNextPage(answer){
-    choices[page] = answer;
+    if(answer != null){
+        choices[page] = answer;
+    }
     page++;
+    console.log(choices);
     if(page == subjects.length){
+        loadSelectStatementsPage();
+    } else if(selectImportantPage.style.display == "block"){
         loadSelectPartiesPage();
     } else if(selectPartiesPage.style.display == "block"){
         loadResultsPage();
@@ -73,13 +124,12 @@ function goToPreviousPage(){
     page--;
     if(page == -1){
         loadIntro();
-    } 
-    // else if(importantQuestions == "block") {
-    //    loadStatement()
-    // } 
-    else if(selectPartiesPage.style.display == "block"){
-        selectPartiesPage.style.display = "none";
+    } else if(selectImportantPage.style.display == "block") {
+        selectImportantPage.style.display = "none";
         loadStatement();
+    } else if(selectPartiesPage.style.display == "block"){
+        selectPartiesPage.style.display = "none";
+        loadSelectStatementsPage();
     } else{
         loadStatement();
     }
